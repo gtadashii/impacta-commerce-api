d7739a9aeeed4dedebd620e5379642475404ff31
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2022-04-21 11:37:11 -0300

    aula 7 unidade 2 minuto 17

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<p> Hello World </p>'
+
+
+@app.route('/products')
+def products():
+    response = jsonify([
+        {
+            'title': 'Caneca Personalizada de Porcelana',
+            'amount': 123.45,
+            'installments': {'number': 3, 'total': 41.15, 'hasFee': True}
+        },
+        {
+            'title': 'Caneca de Tulipa',
+            'amount': 123.45,
+            'installments': {'number': 3, 'total': 41.15}
+        }
+    ])
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
